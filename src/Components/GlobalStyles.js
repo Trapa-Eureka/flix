08c1f7ff44bdea6c3544ff35e3f307156a920756
Trapa-Eureka <son199@hanmail.net>
2021-06-01 23:28:21 +0900

    add Global Styles

---
+++ src/Components/GlobalStyles.js
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+// 여기에 global로 적용될 스타일을 작성 
+const globalStyles = createGlobalStyle`
+    ${reset};
+    a {
+        text-decoration: none;
+        color: inherit;
+    }
+    * {
+        box-sizing: border-box;
+    }
+    body {
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        font-size: 12px;
+        background-color: rgba(20, 20, 20, 1);
+        color: white;
+        padding-top: 50px;
+    }
+`;
+
+export default globalStyles;
