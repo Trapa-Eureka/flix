d7103719a68d1682cb29693526737c7de6aee9fd
Trapa-Eureka <son199@hanmail.net>
2021-06-05 01:53:37 +0900

    Home Container

--- src/Routes/Home/HomeContainer.js
+++ src/Routes/Home/HomeContainer.js
@@ -1,6 +1,7 @@
 import React from "react";
 // eslint-disable-next-line
 import HomePresenter from "./HomePresenter";
+import { moviesApi } from "api";
 
 export default class extends React.Component {
     state = {
@@ -11,9 +12,31 @@ export default class extends React.Component {
         loading: true
     };
 
+    async componentDidMount() {
+        try {
+            const {data:{results: nowPlaying}} = await moviesApi.nowPlaying();
+            const {data:{results: upcoming}} = await moviesApi.upcoming();
+            const {data:{results: popular}} = await moviesApi.popular();
+            this.setState({
+                nowPlaying,
+                upcoming,
+                popular
+            })
+        } catch {
+            this.setState({
+                error: "didn't get any information of Movies"
+            });
+        } finally {
+            this.setState({
+                loading: false
+            });
+        }
+    }
+
     render() {
         // eslint-disable-next-line
         const { nowPlaying, upcoming, popular, error, loading } = this.state;
+        // console.log(this.state);
         return (
             <HomePresenter 
                 nowPlaying={nowPlaying}
