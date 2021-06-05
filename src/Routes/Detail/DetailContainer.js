7187c853bf0c8edfe3e813a1e74684aac453cd48
Trapa-Eureka <son199@hanmail.net>
2021-06-06 01:14:46 +0900

    Detail Container #2

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -1,20 +1,47 @@
 import React from "react";
+import { moviesApi, tvApi } from "../../api";
 // eslint-disable-next-line
 import DetailPresenter from "./DetailPresenter";
 
 export default class extends React.Component {
-    state = {
-        result: null,
-        error: null,
-        loading: true
-    };
+    constructor(props) {
+        super(props);
+        const {location: { pathname }} = props;
+        this.state = {
+            result: null,
+            error: null,
+            loading: true,
+            isMovie: pathname.includes("/movie/")
+        };
+    }
 
     async componentDidMount() {
-        const {match:{params:{id}}, history:{push}} = this.props;
+        const {
+            match: {
+                params: { id }
+            }, 
+            history: { push }
+        } = this.props;
+        const { isMovie } = this.state;
         // console.log(parseInt(id));
         const parsedId = parseInt(id);
         if(isNaN(parsedId)) {
             return push("/");
+        }
+        let result = null;
+        try {
+            if(isMovie) {
+                const request = await moviesApi.movieDetail(parsedId);
+                result = request.data;
+            } else {
+                const request = await tvApi.showDetail(parsedId);
+                result = request.data;
+            }
+            // console.log(result);
+        } catch {
+            this.setState({ error: "Can't find anything."})
+        } finally {
+            this.setState({ loading: false, result });
         }
     }
 
@@ -22,6 +49,7 @@ export default class extends React.Component {
         // console.log(this.props);
         // eslint-disable-next-line
         const { result, error, loading } = this.state;
+        console.log(this.state);
         return (
             <DetailPresenter 
                 result={result}
