c41b1d2e92c38cd920c365a10e54ddfca1495ee5
Trapa-Eureka <son199@hanmail.net>
2021-06-05 18:29:28 +0900

    Search Container

--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { moviesApi, tvApi } from "../../api";
 import SearchPresenter from "./SearchPresenter";
 
 export default class extends React.Component {
@@ -10,8 +11,32 @@ export default class extends React.Component {
         loading: false
     };
 
+    handleSubmit = () => {
+        const { searchTerm } = this.state;
+        if(searchTerm !== "") {
+            this.searchByTerm();
+        }
+    };
+
+    searchByTerm = async ()  => {
+        // eslint-disable-next-line
+        const { searchTerm } = this.state;
+        this.setState({ loading: true });
+        try {
+            const {data:{results: movieResults}} = await moviesApi.search(searchTerm);
+            const {data:{results: tvResults}} = await tvApi.search(searchTerm);
+            // console.log(movieResults, tvResults);
+            this.setState({ movieResults, tvResults });
+        } catch {
+            this.setState({ error: "Can't find results." });
+        } finally {
+            this.setState({ loading: false });
+        }
+    }
+
     render() {
         const { movieResults, tvResults, searchTerm, error, loading } = this.state;
+        // console.log(this.state);
         return (
             <SearchPresenter
                 movieResults={movieResults}
@@ -19,6 +44,7 @@ export default class extends React.Component {
                 searchTerm={searchTerm}
                 loading={loading}
                 error={error}
+                handleSubmit={this.handleSubmit}
             />
         );
     }
