4ac5ef8af8c4e49af483b07d45c3cb75114ff89d
Trapa-Eureka <son199@hanmail.net>
2021-06-06 23:33:46 +0900

    Search Presenter Component

---
+++ src/Components/Loader.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    font-size: 25px;
+    color: rgba(225,225,225,0.3);
+`;
+
+export default () => <Container>Loading</Container>
