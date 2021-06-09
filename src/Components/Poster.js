cdea18cb4f591fca874ed371d17039d9b5b53f51
Trapa-Eureka <son199@hanmail.net>
2021-06-10 00:31:51 +0900

    Poster Component #2

--- src/Components/Poster.js
+++ src/Components/Poster.js
@@ -3,29 +3,88 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-const Container = styled.div``;
+const Container = styled.div`
+    font-size: 12px;
+`;
 
-const ImageContainer = styled.div``;
+const Image = styled.div`
+    background-image: url(${props => props.bgUrl});
+    height: 180px;
+    background-size: cover;
+    border-radius: 4px;
+    background-position: center center;
+    transition: opacity 0.1s linear;
+`;
 
-const Image = styled.div``;
+const Rating = styled.span`
+    bottom: 5px;
+    position: absolute;
+    opacity: 0;
+    transition: opacity 0.1s linear;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 5px;
+    letter-spacing: 1.5px;
+    font-weight: 500;
+    font-size: 11px;
+    span {
+        font-size: 10px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+`;
 
-const Rating = styled.span``;
+const ImageContainer = styled.div`
+    margin-bottom: 5px;
+    position: relative;
+    &:hover {
+        ${Image} {
+            opacity: 0.3;
+        }
+        ${Rating} {
+            opacity: 1;
+        }
+    }
+`;
 
-const Title = styled.span``;
+const Title = styled.span`
+    display: block;
+    margin-bottom: 3px;
+    font-size: 12px;
+    line-height: 14px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    width: 100%;
+`;
 
-const Year = styled.span``;
+const Year = styled.span`
+    font-size: 10px;
+    color: rgba(255, 255, 255, 0.5);
+`;
 
 const Poster = ({id, imageUrl, title, rating, year, isMovie = false}) => (
     <Link to={isMovie ? `/movie/${id}` : `/show/${id}`}>
         <Container>
             <ImageContainer>
-                <Image bgUrl={imageUrl} />
+                <Image 
+                    bgUrl={
+                        imageUrl
+                            ? `https://image.tmdb.org/t/p/w300${imageUrl}`
+                            : require("../assets/noPoster.png").default
+                    } 
+                />
                 <Rating>
-                    <span role="img" aria-label="rating">⭐️</span>
+                    <span role="img" aria-label="rating">⭐️</span>{" "}
                     {rating}/10
                 </Rating>
             </ImageContainer>
-            <Title>{title}</Title>
+            <Title>
+                {title.length > 18 ? `${title.substring(0, 18)}...` : title}
+            </Title>
             <Year>{year}</Year>
         </Container>
     </Link>
